20cfcdd9482ffffc422b9bc5896a2222d8ac2973
fedex3 <federicoianamor@gmail.com>
2023-05-09 17:33:00 +1000

    Initial Commit

---
+++ Digital Pantry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Digital Pantry
+//
+//  Created by Federico Amor on 9/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
