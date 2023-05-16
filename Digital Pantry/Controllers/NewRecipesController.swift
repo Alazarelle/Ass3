a49b51d5ed8324cf025eb59efedb3a678106e07e
fedex3 <federicoianamor@gmail.com>
2023-05-16 18:42:14 +1000

    Integration with Open AI working

---
+++ Digital Pantry/Controllers/NewRecipesController.swift
@@ -0,0 +1,47 @@
+//
+//  NewRecipesController.swift
+//  Digital Pantry
+//
+//  Created by Federico Amor on 16/5/2023.
+//
+
+import Foundation
+
+import UIKit
+import OpenAIKit
+
+class NewRecipesController: UIViewController {
+    
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var generateRecipeButton: UIButton!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    private var models = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func generateRecipeButtonPressed(_ sender: UIButton) {
+        if let text = inputTextField.text, !text.isEmpty {
+            APICaller.shared.getResponse(input: text) { [weak self] result in
+                switch result{
+                case .success(let output):
+                    DispatchQueue.main.async {
+                        print("success")
+
+                        self?.outputLabel.text = output
+                    }
+
+                case .failure:
+                    print("failed")
+                }
+                
+            }
+        }
+    }
+    
+}
