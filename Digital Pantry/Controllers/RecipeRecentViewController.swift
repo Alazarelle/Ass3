81e4e0424b14ca8503300425462c48467e029f60
fedex3 <federicoianamor@gmail.com>
2023-05-29 19:22:17 +1000

    table view done for recent recipes

--- Digital Pantry/Controllers/RecipeRecentViewController.swift
+++ Digital Pantry/Controllers/RecipeRecentViewController.swift
@@ -8,16 +8,44 @@
 import UIKit
 
 class RecipeRecentViewController: UIViewController {
+    let recipes = readRecipes()
 
-    @IBOutlet weak var RecentrecipesTextView: UITextView!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let recipes = readRecipes()
-        RecentrecipesTextView.text = recipes.description
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
 }
 
+extension RecipeRecentViewController:UITableViewDelegate {
+    func tableView(_ tableView:UITableView, didSelectRowAt indexPath: IndexPath){
+        //what should the app do when user selecting row at a certain index?
+    }
+}
+
+extension RecipeRecentViewController:UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //as a table view, how many cells should i displayin this section
+        return recipes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //as a table view, what cell should i display when user is at this index?
+        
+        //dequed a reusable cell from the table view
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        //update the Ui for this cell
+        let recipe = recipes[indexPath.row]
+        cell?.textLabel?.text = recipe.recipeName
+        cell?.textLabel?.textColor = .orange
+        //return the cell to Table View
+        return cell!
+    }
+}
 
